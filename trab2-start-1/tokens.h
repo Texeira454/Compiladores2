d8a9e03933c3d1f6a87eda4a3fdc88f19ab6ad76
Texeira454 <felipetgon@outlook.com>
2026-03-16 20:33:28 -0300

    Add files via upload

---
+++ trab2-start-1/tokens.h
@@ -0,0 +1,16 @@
+#define NUM 256
+#define ID 257
+#define INT 258
+#define GE 259
+#define IF 260
+#define FOR 261
+#define WHILE 262
+#define CHAR 263
+#define VOID 264
+#define FLOAT 265
+#define ELSE 266
+#define GR 267
+#define RETURN 268
+#define NOT 269
+#define OR 270
+#define AND 271
